471616dd267b97082e0e3a2bcb0e8c28766b95c9
dqhntt <48117687+dqhntt@users.noreply.github.com>
2022-03-31 04:36:31 -0700

    Prepare files for unit tests
    For #37.
    Testing `util.file.h` next.

--- src/util.file.h
+++ src/util.file.h
@@ -8,8 +8,40 @@
 
 namespace util::file {
 
-std::vector<City> read_data(const std::string& filename);
-void              write_data(const std::vector<City>& data, const std::string& filename);
+///////////////////////////////////////////////////////////////////////////////
+//
+// Description:
+//    Parse data from a database file of `City` records.
+// Pre-condition:
+//    - `filename` points to a formatted existing file.
+//      +) e.g. "../data/database.txt"
+//    - All lines in file excluding headers have the following format:
+//      +) city name, province abbreviation, province name,
+//         latitude, longitude, population, population density
+//      +) Use semicolons ';' as delimiters.
+//      +) Have exactly 6 semicolons per line.
+// Post-condition:
+//    - Ignore `skip_lines` number of rows before parsing. Default is 1.
+//    - Return a vector of all records in the file excluding last empty lines.
+std::vector<City> read_data(const std::string& filename, int skip_lines = 1);
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Description:
+//    Write database records to file.
+// Pre-condition:
+//    - `filename` points to an existing file that can be overwritten.
+//      +) e.g. "../data/database.txt"
+// Post-condition:
+//    - Overwrite existing file pointed to by `filename`.
+//    - Semicolons ';' are used as delimiters.
+//    - One header row will be created with the names of all fields.
+//    - All lines after that will have the following format:
+//      +) city name, province abbreviation, province name,
+//         latitude, longitude, population, population density
+//      +) Fields that are floating point numbers will have the
+//         number of digits after the decimal point == `precision`. Default is 5.
+void write_data(const std::vector<City>& data, const std::string& filename, int precision = 5);
 
 } // namespace util::file
 
--- src/util.h
+++ src/util.h
@@ -4,4 +4,20 @@
 #include "util.file.h"
 #include "util.RAII.h"
 
+#include <string>
+
+namespace util {
+
+inline bool is_number(const std::string& str)
+{
+    try {
+        const double n = std::stod(str);
+        return (n == n); // NaN != NaN
+    } catch (...) {
+        return false;
+    }
+}
+
+} // namespace util
+
 #endif // UTIL_H
