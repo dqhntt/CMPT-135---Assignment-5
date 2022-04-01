b2c6920f2eee155998960b5aa77a950e3964cd5c
henryc01 <aca189@sfu.ca>
2022-04-01 00:22:35 -0700

    Fixing the makefile bug

--- src/Database.cpp
+++ src/Database.cpp
@@ -1,9 +1,10 @@
 #include "Database.h"
 #include "util.file.h"
-
+#include "cmpt_error.h"
 #include <algorithm>
 #include <iostream>
 using namespace std;
+
 
 Database::Database(const string& db_filename)
     : _db_filename(db_filename)
@@ -22,7 +23,7 @@ Database::~Database() // Destructor must never let exception escape.
     }
 }
 
-void Database::open(const std::string& target_filename)
+void Database::open(const string& target_filename)
 {
     if (!_db_filename.empty()) {
         util::file::write_data(_cities, _db_filename);
@@ -33,3 +34,148 @@ void Database::open(const std::string& target_filename)
 }
 
 // TODO
+
+//////////////////////////////////////////////////////////////////////////////////////
+//Finds the target data by the according data types and returns the index
+//If it finds the city in database, append the city to the result
+//If it does not find it, return an empty vector
+vector<City> Database::cities_matching_string(const Field& field, bool substring, const string& target_data) const{
+    vector<City> result;
+    for(City c : _cities){
+        string target;
+        //Choose the target depending on data typeSS
+        if(field == Field::city_name){
+            target = c.name;
+        } else if (field == Field::province_id){
+            target = c.province_id;
+        } else if (field == Field::province){
+            target = c.province;
+        } else {
+            cmpt::error("cities_matching_string: Invalid datatype.\n");
+        }
+
+        //Compare the whole string or substring depending on the boolean "substring"
+        if(substring == true){ //if this is a substring input
+            //Note the find() method below is from <string> class
+            if(target.find(target_data) != string::npos) 
+                result.push_back(c);
+        } else{ //has to match the exact string
+            if(target == target_data) 
+                result.push_back(c);
+        }        
+    }// end of for loop
+    return result;
+}
+
+//Find the city by number inputs. If we compare the exact number, only data1 is used.
+//
+vector<City> Database::cities_in_number_range(const Field& field, double range_low, double range_high) const{
+    vector<City> result;
+    for(City c : _cities){
+        double target;
+        //Choose the target depending on data type
+        if(field == Field::latitude){
+            target = c.latitude;
+        } else if (field == Field::longitude){
+            target = c.longitude;
+        } else if (field == Field::population){
+            target = (double)c.population;
+        } else if (field == Field::population_density){
+            target = c.population_density;
+        } else {
+            cmpt::error("cities_in_number_range: Invalid datatype.\n");
+        }
+        //Compare the exact number or range of number
+        if((target >= range_low) && (target <= range_high)) 
+            result.push_back(c);
+    }// end of for loop
+    return result;
+}
+
+vector<City> Database::cities_matching_exact_number(const Field& field, double target_num) const{
+    return cities_in_number_range(field, target_num, target_num);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+//Helper function made for sorting the vector<City>. 
+//The comparison logic is depending on the field
+bool less_than(const City& city1 , const City& city2, const Field& field){
+    switch(field){
+        case Field::city_name:
+            return (city1.name < city2.name);
+        case Field::province_id:
+            return (city1.province_id < city2.province_id);
+        case Field::province:
+            return (city1.province < city2.province);
+        case Field::latitude:
+            return (city1.latitude < city2.latitude);
+        case Field::longitude:
+            return (city1.longitude < city2.longitude);
+        case Field::population:
+            return (city1.population < city2.population);
+        case Field::population_density:
+            return (city1.population_density < city2.population_density);
+        default: 
+            cmpt::error("bool less_than: Invalid field.\n");
+    }
+    return true; //Never going to reach this line
+}
+
+//Sorts the data:
+//In ascending order when reversed_mode is false, and
+//descending order when reversed_mode is true.
+void Database::sort_cities(const Field& field, bool reversed_mode){
+    if(reversed_mode){ //the order is reversed, we are sorting in descending order
+        std::sort(_cities.begin(), _cities.end(),
+            [field](const City& c1 , const City& c2) -> bool{return !less_than(c1,c2,field);});
+    } else{// sorting in ascending order
+        std::sort(_cities.begin(), _cities.end(),
+            [field](const City& c1 , const City& c2) -> bool{return less_than(c1,c2,field);});
+    }
+}
+
+
+bool Database::exists_record(const City& city) const{
+    for(City c : _cities){
+        //if the city is already in the list
+        if(c == city){
+            return true;
+        }
+    }
+    return false;
+}
+
+//Add a new city to the database
+//If the city is already in the database, do nothing
+void Database::add_city(const City& city){
+    if(!exists_record(city))
+        _cities.push_back(city);
+}
+
+//Delete a city from database
+void Database::delete_city(const City& city){
+    int target_index = 0;
+    while(target_index < _cities.size()){
+        if(_cities[target_index] == city){
+            _cities.erase(_cities.begin() + target_index);
+            return;
+        }
+        target_index++;
+    }
+    //if the loop is finished, that means we did not find the target 
+    //print a message to show users theres an error
+    cmpt::error("Unable to delete because the city is not in the list.\n");
+}
+
+//Deletes a list of cities
+void Database::delete_cities(const vector<City>& cities){
+    for(City c : cities){
+        delete_city(c);
+    }
+}
+
+//Returns the underlying _cities vector
+vector<City> Database::get_cities() const{
+    return _cities;
+}
--- src/Database.h
+++ src/Database.h
@@ -4,6 +4,7 @@
 #include "City.h"
 #include <string>
 #include <vector>
+using namespace std;
 
 // TODO: Implement this class.
 
@@ -19,6 +20,7 @@ enum class Field {
 
 class Database {
 public:
+
     Database() = default;
 
     ///////////////////////////////////////////////////////////////////////////////
@@ -46,38 +48,51 @@ public:
     //    File at `target_filename` is opened and loaded into memory.
     void open(const std::string& target_filename);
 
+
+
     // TODO
 
-    // Question: (1 mark) When searching for strings, one kind of search
+    // Question: (1 mark) When searching for strings, one kind of search 
     // should print all records where the typed-in string from the user
     // exactly matches the appropriate field of a record.
 
     // Finds the target data by name
-    // The Boolean "substring" determines if we want to compare substring or not
-    std::vector<City> cities_matching_string(const Field& field, 
-                                             bool substr_mode, const string& target_str) const;
+    //There are 3 datatypes in fine_by_string: "name", "province_id", "province"
+    //The Boolean "substring" determines if we want to compare substring or not
+    vector<City> cities_matching_string(const Field& field, bool substring, const string& target_data) const;
 
-    std::vector<City> cities_in_number_range(const Field& field, 
-                                             double range_low, double range_high) const;
-    std::vector<City> cities_matching_exact_number(const Field& field, double target_num) const;
+    
+    // find num in range : function1
+    // find exact match: function1(0, vector.size())
 
+    // exact match : low = high = num
+    vector<City> cities_in_number_range(const Field& field, double range_low, double range_high) const;
+    vector<City> cities_matching_exact_number(const Field& field, double target_num) const;
+
+    //Checks if a city exists in the database
     bool exists_record(const City& city) const;
 
     // Add a new city to the database
-    void add_city(const City& city); // push-back, sort
+    void add_city(const City& city); //push-back, sort
 
-    // Delete a city from database
+    //Delete a city from database
+    void delete_city(const City& city);
+
+    //Delete a list of cities from database
     void delete_cities(const vector<City>& cities);
 
-    // (1 mark) For each string field in your record, allow the user
+    // (1 mark) For each string field in your record, allow the user 
     // to list all records in alphabetical order.
 
-    void sort(const Field& field, bool reversed_mode);
+    void sort_cities(const Field& field, bool reversed_mode);
+
+    //Gets the underlying city in the class. 
+    vector<City> get_cities() const;
 
 private:
     std::string       _db_filename;
     std::vector<City> _cities;
-
 }; // class Database
+
 
 #endif // DATABASE_H
