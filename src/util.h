9072baf322304ebb3ef4b0ceb175bfdeb0a16655
henryc01 <aca189@sfu.ca>
2022-03-31 21:04:21 -0700

    added new files

--- src/util.h
+++ src/util.h
@@ -1,7 +1,20 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-#include "util.file.h"
 #include "util.RAII.h"
+#include "util.file.h"
+
+#include <string>
+
+namespace util {
+
+inline bool is_number(const std::string& str) {
+    try {
+        const double n = std::stod(str);
+        return (n == n); // NaN != NaN
+    } catch (...) { return false; }
+}
+
+} // namespace util
 
 #endif // UTIL_H
